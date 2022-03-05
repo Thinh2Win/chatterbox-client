9a6f4dd56e87874792f3ccec2f398614c3d3686c
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-03-05 15:32:47 -0800

    Complete

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -7,6 +7,11 @@ var Messages = {
   // TODO: Define how you want to store your messages.
   _data: null,
 
+  assign: function(data) {
+    Messages._data = data;
+  }
+
+
   // TODO: Define methods which allow you to retrieve from,
   // add to, and generally interact with the messages.
 
